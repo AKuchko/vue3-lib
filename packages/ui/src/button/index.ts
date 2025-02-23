01feb007cb0aea9950d4ce32b1b695efd8242603
AKuchko <vasco_da_gama1469@mail.ru>
2025-02-23 22:52:35 +0700

    fix (@ui/button): import withInstall function

--- packages/ui/src/button/index.ts
+++ packages/ui/src/button/index.ts
@@ -1,18 +1,5 @@
-import { App, Plugin } from 'vue';
+import withInstall from '../utils/with-install';
 import _Button from './button.vue';
-
-type SFCWithInstall<T> = T & Plugin;
-
-const withInstall = <T>(component: T): SFCWithInstall<T> => {
-  const componentInstall = (app: App) => {
-    const name = (component as any).name;
-    app.component(name, component as SFCWithInstall<T>);
-  };
-
-  (component as SFCWithInstall<T>).install = componentInstall;
-
-  return component as SFCWithInstall<T>;
-}
 
 export const Button = withInstall(_Button);
 export default Button;
