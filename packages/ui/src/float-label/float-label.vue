61308c313b22bf3dd5e577d9c53e5025b52ad26a
AKuchko <vasco_da_gama1469@mail.ru>
2025-02-23 22:54:04 +0700

    fix (@ui/float-label): styles for input, add error state, ignore on disabled input

---
+++ packages/ui/src/float-label/float-label.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import './style/index.scss';
+import { defineOptions } from 'vue';
+
+defineOptions({
+  name: 'ak-float-label',
+});
+
+interface FloatLabelProps {
+  label?: string,
+};
+
+const props = defineProps<FloatLabelProps>();
+</script>
+
+<template>
+  <label class="ak-float-label">
+    <span class="ak-float-label__label">
+      {{ props.label }}
+    </span>
+    <slot />
+  </label>
+</template>
---
+++ packages/ui/src/float-label/index.ts
@@ -0,0 +1,5 @@
+import withInstall from '../utils/with-install';
+import _FloatLabel from './float-label.vue';
+
+export const FloatLabel = withInstall(_FloatLabel);
+export default FloatLabel;
